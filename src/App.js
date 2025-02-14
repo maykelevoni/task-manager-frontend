d414ca007978268addb2c5c95f1802dcd56e4fd7
maykelevoni <maykelevoni@gmail.com>
2025-02-13 19:49:14 -0500

    feat: add TaskItem component

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import TaskItem from "./components/TaskItem";
+
+const App = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: "1",
+            description: "Estudar Programacao",
+            isCompleted: false,
+        },
+        {
+            id: "2",
+            description: "Ler",
+            isCompleted: true,
+        },
+    ]);
+    return (
+        <>
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+            ))}
+        </>
+    );
+};
+
+export default App;
